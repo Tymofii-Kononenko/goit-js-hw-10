bfb89d88a7139580673b26c4bffc4468960341f9
Tymofii Kononenko <timajw95@gmail.com>
2024-11-23 19:24:52 +0100

    03

---
+++ src/js/1-timer.js
@@ -0,0 +1,36 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+const buttonStart = document.querySelector('[data-start]')
+const days = document.querySelector('[data-days]')
+const hours = document.querySelector('[data-hours]')
+const minutes = document.querySelector('[data-minutes]')
+const seconds = document.querySelector('[data-seconds]')
+const inputPicker = document.querySelector('#datetime-picker')
+
+
+let userSelectedDate = null;
+
+buttonStart.disabled = true;
+
+
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+        userSelectedDate = selectedDates[0].getTime();
+        buttonStart.disabled = false;
+    },
+};
+
+flatpickr(inputPicker, options);
+
+
+
+
+
+
